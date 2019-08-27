70a170381ec3695e34b22ce3ff88034b6de1f546
Harith <harixth@yahoo.com>
2019-08-27 17:01:12 +0800

    leap submission

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,5 +1,6 @@
-function isLeapYear( /* Parameters go here */ ) {
+function isLeapYear(year : number) {
     // Your code here
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
 
 export default isLeapYear
